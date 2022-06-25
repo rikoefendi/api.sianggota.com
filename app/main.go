9a4714eff5dcddead478a44b85c4dbf73e1829ae
Efendi <riko@Efendis-iMac.local>
2022-06-25 20:30:51 +0700

    setup server

--- app/main.go
+++ app/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		message := `
+		<center>==================================================================================
+		<p><h2>sianggota</h2>
+		<p>Sistem Informasi Anggota
+		<p>==================================================================================</center>`
+		return c.HTML(200, message)
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	go func() {
+		address := fmt.Sprintf("localhost:%d", 3000)
+
+		if err := e.Start(address); err != nil {
+			log.Info("shutting down the server")
+		}
+	}()
+	// Wait for interrupt signal to gracefully shutdown the server with
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	// a timeout of 10 seconds to shutdown the server
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
 }
